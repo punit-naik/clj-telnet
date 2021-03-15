4cec631e74bf6b7dc67ee841d286898c391cc301
Punit Naik <naik.punit44@gmail.com>
2021-03-16 02:39:59 +0530

    added init, destroy and send-message fns

--- src/clj_telnet/core.clj
+++ src/clj_telnet/core.clj
@@ -1,6 +1,31 @@
-(ns clj-telnet.core)
+(ns clj-telnet.core
+  (:require [gniazdo.core :as ws]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init
+  "Sets up connection to a WS server using the URL provided
+   Returns the socket connection object"
+  [{:keys [on-receive on-connect on-close on-error url]
+    :or {url "ws://echo.websocket.org"
+         on-receive #(println (str "Received message from the server: \"" % "\""))
+         on-connect #(println "Connected to server:" (-> % .getRequestURI .toString))
+         on-close (fn [code reason] (println (str "Closed connection to the server with code " code " => " reason)))
+         on-error #(println "Error occured" (ex-message %))}}]
+  (ws/connect url
+   :on-receive on-receive
+   :on-close on-close
+   :on-error on-error
+   :on-connect on-connect))
+
+(defn send-message
+  [socket message]
+  (ws/send-msg socket message))
+
+(defn destroy
+  [socket]
+  (ws/close socket))
+
+(comment
+  (def s (init nil))
+  (destroy s)
+  s
+  (send-message s "test message"))
