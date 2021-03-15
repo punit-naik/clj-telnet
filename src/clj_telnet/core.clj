ab9f7510a0f69877420617f9234981b7491528ec
Punit Naik <naik.punit44@gmail.com>
2021-03-16 03:16:13 +0530

    added main fn

--- src/clj_telnet/core.clj
+++ src/clj_telnet/core.clj
@@ -8,7 +8,7 @@
     :or {url "ws://echo.websocket.org"
          on-receive #(println (str "Received message from the server: \"" % "\""))
          on-connect #(println "Connected to server:" (-> % .getRequestURI .toString))
-         on-close (fn [code reason] (println (str "Closed connection to the server with code " code " => " reason)))
+         on-close (fn [code reason] (println (str "\nClosed connection to the server with code " code " => " reason)))
          on-error #(println "Error occured:" (ex-message %))}}]
   (ws/connect url
    :on-receive on-receive
@@ -23,3 +23,21 @@
 (defn destroy
   [socket]
   (ws/close socket))
+
+(defn user-prompt
+  [socket]
+  (println "Type message to send Or CTRL+C to exit")
+  (send-message socket (read-line))
+  (Thread/sleep 1000))
+
+(defn -main
+  [& args]
+  (let [socket (init nil)]
+    (Thread/sleep 500)
+    (loop [s socket]
+      (when-not (nil? s)
+        (user-prompt s)
+        (recur s)))
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook
+         (Thread. (fn [] (destroy socket)))))))
