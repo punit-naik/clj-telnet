0a99f6e30c6670db5cdabb259cb9009ecd2544d9
Punit Naik <naik.punit44@gmail.com>
2021-03-16 02:39:42 +0530

    added deps and plugins

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [stylefruits/gniazdo "1.2.0"]]
+  :profiles
+  {:uberjar {:main allstreet.editor.server.core
+             :aot :all}
+   :dev {:plugins [[lein-kibit "0.1.8"]
+                   [lein-cloverage "1.2.2"]
+                   [lein-codox "0.10.7"]]}}
   :repl-options {:init-ns clj-telnet.core})
