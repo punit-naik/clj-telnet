801d97d77a76c9d4753f6c7d3b53c6643ce0f67e
Punit Naik <naik.punit44@gmail.com>
2021-03-16 02:40:12 +0530

    added tests for init, destroy and send-message fns

--- test/clj_telnet/core_test.clj
+++ test/clj_telnet/core_test.clj
@@ -1,7 +1,30 @@
 (ns clj-telnet.core-test
-  (:require [clojure.test :refer :all]
-            [clj-telnet.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [clj-telnet.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest init-test
+  (testing "Init fn"
+    (let [x (atom false)
+          on-connect (fn [_] (reset! x true))
+          s (core/init {:on-connect on-connect})]
+      (is (true? @x))
+      (core/destroy s))))
+
+(deftest destroy-test
+  (testing "Destroy fn"
+    (let [x (atom false)
+          on-close (fn [_ _] (reset! x true))
+          s (core/init {:on-close on-close})]
+      (core/destroy s)
+      (is (true? @x)))))
+
+(deftest send-message-test
+  (testing "Send message fn"
+    (let [message "test message"
+          x (atom nil)
+          on-receive (fn [m] (reset! x m))
+          s (core/init {:on-receive on-receive})]
+      (core/send-message s message)
+      (Thread/sleep 1000)
+      (is (= @x message))
+      (core/destroy s))))
